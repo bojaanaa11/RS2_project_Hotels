a7aa1dba06d3fcab0284d9e55c2b7f823afe7dc7
bojana <bojanaobradovic011@gmail.com>
2024-04-04 14:19:59 +0200

    Added new entity 'Reservation' for Reservation.API

---
+++ hotels/Services/Reservations/Reservations.API/Entities/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace Reservations_API.Entities
+{
+    public class Reservation
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string BookingDateTime { get; set; }
+        public string StartDateTime { get; set; }
+        public string EndDateTime { get; set; }
+        public string Status { get; set; }
+    }
+}
