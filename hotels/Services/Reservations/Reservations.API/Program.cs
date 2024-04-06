244c1a6efde2d38ba59e64718aaa7ca7c03b2336
bojana <bojanaobradovic011@gmail.com>
2024-04-06 11:11:40 +0200

    Added dependency injection for Reservation service

--- hotels/Services/Reservations/Reservations.API/Program.cs
+++ hotels/Services/Reservations/Reservations.API/Program.cs
@@ -1,6 +1,11 @@
+using Reservations.API.Data;
+using Reservations.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IReservationsContext, ReservationsContext>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
