db61a4aa22c7be6a06b579444a8ccc3537be9f5c
AleksandraBiocanin <aleksandrabiocanin6@gmail.com>
2024-04-08 22:57:58 +0200

    Added entities to RoomManaging service

---
+++ hotels/Services/ManagingRooms/RoomManaging.API/Entities/Hotel.cs
@@ -0,0 +1,13 @@
+﻿namespace Room_Managing_API.Entities
+
+    
+{
+    public class Hotel
+    {
+        public string ID { get; set; }
+
+        public IEnumerable<Room> Rooms { get; set; } = new List<Room>();
+
+       
+    }
+}
---
+++ hotels/Services/ManagingRooms/RoomManaging.API/Entities/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace Room_Managing_API.Entities
+{
+    public class Room
+    {
+        public string ID { get;  set; }
+
+        public bool Status { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
