d88a15a87e3a6057e6e988b1586e8f26ca68c6b6
AleksandraBiocanin <aleksandrabiocanin6@gmail.com>
2024-04-23 22:55:28 +0200

    Changes to RoomManaging
    Added common project
    Added grpc project
    Added protobuff file for grpc with Reservations api
    Registered grpc as server for communication with Reservations service
    Adjusted docker-compose

--- hotels/Services/ManagingRooms/RoomManaging.Common/Entities/Hotel.cs
+++ hotels/Services/ManagingRooms/RoomManaging.Common/Entities/Hotel.cs
@@ -6,6 +6,8 @@
     {
         public string ID { get; set; }
 
+        public string Name { get; set; }
+
         public IEnumerable<Room> Rooms { get; set; } = new List<Room>();
 
        
---
+++ hotels/Services/ManagingRooms/RoomManaging.GRPC/Services/HotelsRoomsService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using RoomManaging.GRPC;
+using RoomManaging.GRPC.Protos;
+
+namespace RoomManaging.GRPC.Services
+{
+    public class HotelsRoomsService : HotelsRoomsProtoService.HotelsRoomsProtoServiceBase
+    {
+        public override Task<GetAllHotelsResponse> GetAllHotels(GetAllHotelsRequest request, ServerCallContext context)
+        {
+            return base.GetAllHotels(request, context);
+        }
+
+        public override Task<GetAllRoomsResponse> GetAllRooms(GetAllRoomsRequest request, ServerCallContext context)
+        {
+            return base.GetAllRooms(request, context);
+        }
+    }
+}
