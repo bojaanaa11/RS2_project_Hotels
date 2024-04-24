d9527591c55c1ef2422accc8fe31e5fe53826dc2
bojana <bojanaobradovic011@gmail.com>
2024-04-24 16:39:32 +0200

    Implemented grpc service for handling client end of communication with RoomManaing service in Reservations service

---
+++ hotels/Services/Reservations/Reservations.API/GrpcServices/HotelsRoomsGrpcService.cs
@@ -0,0 +1,27 @@
+﻿using RoomManaging.GRPC.Protos;
+
+namespace Reservations.API.GrpcServices
+{
+    public class HotelsRoomsGrpcService
+    {
+        private readonly HotelsRoomsProtoService.HotelsRoomsProtoServiceClient _hotelsroomsProtoServiceClient;
+
+        public HotelsRoomsGrpcService(HotelsRoomsProtoService.HotelsRoomsProtoServiceClient hotelsroomsProtoServiceClient)
+        {
+            _hotelsroomsProtoServiceClient = hotelsroomsProtoServiceClient ?? throw new ArgumentNullException(nameof(hotelsroomsProtoServiceClient));
+        }
+
+        public async Task<GetAllHotelsResponse> GetAllHotels()
+        {
+            var hotelsRequest = new GetAllHotelsRequest();
+            return await _hotelsroomsProtoServiceClient.GetAllHotelsAsync(hotelsRequest);
+        }
+
+        public async Task<GetAllRoomsResponse> GetAllRooms(string Id)
+        {
+            var roomsRequest = new GetAllRoomsRequest();
+            roomsRequest.Id = Id;
+            return await _hotelsroomsProtoServiceClient.GetAllRoomsAsync(roomsRequest);
+        }
+    }
+}
