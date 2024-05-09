85708edb534d9a947600255a53dd6db84b01edc2
bojana <bojanaobradovic011@gmail.com>
2024-05-09 23:15:13 +0200

    Added proto file, started grpc service implementation, containerized grpc project

--- hotels/Services/Reservations/Reservations.GRPC/Program.cs
+++ hotels/Services/Reservations/Reservations.GRPC/Program.cs
@@ -1,3 +1,5 @@
+using Reservations.Common.Data;
+using Reservations.Common.Repositories;
 using Reservations.GRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,10 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddScoped<IReservationsContext, ReservationsContext>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<UsersReservationService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ hotels/Services/Reservations/Reservations.GRPC/Services/UsersReservationService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using Reservations.Common.Repositories;
+using Reservations.GRPC.Protos;
+
+namespace Reservations.GRPC.Services
+{
+    public class UsersReservationService : UsersReservationProtoService.UsersReservationProtoServiceBase
+    {
+        private readonly IReservationRepository _repository;
+        private readonly ILogger<UsersReservationService> _logger;
+
+        public UsersReservationService(IReservationRepository repository, ILogger<UsersReservationService> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger)); ;
+        }
+
+        public override Task<GetReservationResponse> GetReservation(GetReservationRequest request, ServerCallContext context)
+        {
+            return base.GetReservation(request, context);
+        }
+    }
+}
