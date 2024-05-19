b87608994f62f7bb86c3a5483fbbaa9b5d886a26
bojana <bojanaobradovic011@gmail.com>
2024-05-19 03:13:50 +0200

    [Security] Setup EFC, create context, extensions

---
+++ hotels/Security/IdentityServer/Extensions/IdentityExtensions.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer.Data;
+using IdentityServer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnectionString"), builder =>
+                {
+                    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+                });
+            }
+            );
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
--- hotels/Security/IdentityServer/Program.cs
+++ hotels/Security/IdentityServer/Program.cs
@@ -1,3 +1,5 @@
+using IdentityServer.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication();
+
+builder.Services.ConfigurePersistence(builder.Configuration);
+builder.Services.ConfigureIdentity();
 
 var app = builder.Build();
 
@@ -16,6 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
