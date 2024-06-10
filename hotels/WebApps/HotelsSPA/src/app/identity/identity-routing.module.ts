9ea33a21c75967511f1020367eca9b67716d2041
bojana <bojanaobradovic011@gmail.com>
2024-06-10 22:46:36 +0200

    [Frontend] Added guard for case of not authenticated user

--- hotels/WebApps/HotelsSPA/src/app/identity/identity-routing.module.ts
+++ hotels/WebApps/HotelsSPA/src/app/identity/identity-routing.module.ts
@@ -4,11 +4,12 @@ import { IdentityComponent } from './identity.component';
 import { LoginFormComponent } from './feature-authentication/login-form/login-form.component';
 import { UserProfileComponent } from './feature-user-info/user-profile/user-profile.component';
 import { LogoutComponent } from './feature-authentication/logout/logout.component';
+import { NotAuthenticatedGuard } from '../shared/guards/not-authenticated.guard';
 
 const routes: Routes = [
   { path: '', component: IdentityComponent },
   { path: 'login', component: LoginFormComponent},
-  { path: 'profile', component: UserProfileComponent},
+  { path: 'profile', component: UserProfileComponent, canActivate: [NotAuthenticatedGuard]},
   { path: 'logout', component: LogoutComponent}
 ];
 
