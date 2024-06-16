0520c5bb36e0fd9b491f76e4126d0ab31c9bf6a2
AleksandraBiocanin <aleksandrabiocanin6@gmail.com>
2024-06-16 16:09:00 +0200

    add component for managingrooms

--- hotels/WebApps/HotelsSPA/src/app/app-routing.module.ts
+++ hotels/WebApps/HotelsSPA/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) }];
+const routes: Routes = [
+  {path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
